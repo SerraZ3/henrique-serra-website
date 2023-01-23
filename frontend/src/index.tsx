69fbd655fa89d1fe05df4eb358e6c16e11694c00
SerraZ3 <serra.henrique3@gmail.com>
2023-01-23 10:52:25 -0300

    feat: migrando projeto pra react

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,7 +5,11 @@ import "./index.css";
 import ReactGA from "react-ga";
 const TRACKING_ID = "G-1MR622K5WL"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
